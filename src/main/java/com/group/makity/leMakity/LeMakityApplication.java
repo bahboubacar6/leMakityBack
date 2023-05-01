c835eed43030fb962f52eba5600aa73891dfcee5
bahboubacar6 <bahboubacar6@yahoo.fr>
2023-05-01 22:27:06 +0200

    first commit

---
+++ src/main/java/com/group/makity/leMakity/LeMakityApplication.java
@@ -0,0 +1,13 @@
+package com.group.makity.leMakity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeMakityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeMakityApplication.class, args);
+	}
+
+}
